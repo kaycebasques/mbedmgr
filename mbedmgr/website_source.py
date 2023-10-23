53bc7d6f05fb9c0c6f3db02fdee6fe6372df0868
Kayce Basques <kaycebasques@gmail.com>
2023-10-23 15:19:19 -0700

    feat: Start minification API for WebsiteSource

--- mbedmgr/website_source.py
+++ mbedmgr/website_source.py
@@ -4,42 +4,72 @@ import threading
 import lxml
 import typing
 import uuid
+import time
+import json
+
+class WebpageData(typing.TypedDict):
+    text: str
 
 class WebsiteSource:
     def __init__(self):
-        self._urls = {}
+        self._data = {}
 
     @property
-    def urls(self) -> typing.List[str]:
-        return self._urls
+    def data(self) -> typing.Dict[str, WebpageData]:
+        return self._data
 
-    @urls.setter
-    def urls(self, urls: typing.List[str]) -> None:
-        self._urls = urls
-
-    def scrape_sitemap(self, sitemap_url: str) -> None:
+    def get_pages_from_sitemap(self, sitemap_url: str) -> None:
         response = requests.get(sitemap_url)
         root = lxml.etree.fromstring(response.content)
         namespaces = {'sitemap': 'http://www.sitemaps.org/schemas/sitemap/0.9'}
         urls = [loc.text for loc in root.xpath('//sitemap:loc', namespaces=namespaces)]
         for url in urls:
-            self._urls[url] = {}
+            self._data[url] = {}
 
-    def _scrape(self, url: str) -> None:
-        response = requests.get(url)
-        if not response.ok:
-            return
-        self._urls[url] = {
-            'text': response.text,
-            'type': response.headers.get('Content-Type')
-        }
-
-    def scrape_urls(self) -> None:
+    def scrape_pages(self) -> None:
         threads = []
-        for index, url in enumerate(self._urls):
+        for url in self._data:
             thread_id = str(uuid.uuid4())
-            thread = threading.Thread(target=self._scrape, name=thread_id, args=(url,))
+            thread = threading.Thread(target=self._scrape_page, name=thread_id, args=(url,))
             threads.append(thread)
             thread.start()
         for thread in threads:
             thread.join()
+
+    def minify_pages(self) -> None:
+        threads = []
+        for url in self._data:
+            thread_id = str(uuid.uuid4())
+            thread = threading.Thread(target=self._minify_page, name=thread_id, args=(url,))
+            threads.append(thread)
+            thread.start()
+        for thread in threads:
+            thread.join()
+
+    def save(self) -> None:
+        with open('data.json', 'w') as f:
+            json.dump(self._data, f, indent=4)
+
+    # TODO: Turn this into a caller-defined function.
+    def _minify_page(self, url: str) -> None:
+        soup = bs4.BeautifulSoup(self._data[url]['text'], 'html.parser')
+        main = soup.select('div.main')
+        if len(main) != 1:
+            print('ERROR: Expected page to have exactly 1 div.main element')
+        main = main[0]
+        for script in main.find_all('script'):
+            script.decompose()
+        for style in main.find_all('style'):
+            style.decompose()
+        for link in main.find_all('link'):
+            link.decompose()
+        self._data[url]['text'] = str(main)
+
+    def _scrape_page(self, url: str) -> None:
+        response = requests.get(url)
+        if not response.ok:
+            return
+        self._data[url] = {
+            'text': response.text,
+            'type': response.headers.get('Content-Type')
+        }
